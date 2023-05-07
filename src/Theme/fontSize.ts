06a1db8969d91d7c1b50749186084420ccc72131
jhyoon <jhyooooooon@gmail.com>
2023-05-07 17:43:46 +0900

    style: 테마 지정

---
+++ src/Theme/fontSize.ts
@@ -0,0 +1,10 @@
+export const fontSize = {
+	xxs: 0.625, // 10px
+	xs: 0.75, // 12px
+	sm: 0.875, // 14px
+	base: 1, // 16px
+	lg: 1.125, // 18px
+	xl: 1.25, // 20px
+	xxl: 1.5, // 24px
+	xxxl: 1.875, // 30px
+};
